a98d83af009e49618e6be3356b2d9fbc90f0c134
Emre Nakilcioglu <emre.nakilcioglu@ericsson.com>
2018-08-28 13:22:51 +0300

    initial commit

---
+++ abstractclassdemo/src/abstractclassdemo/AbstractFactory.java
@@ -0,0 +1,12 @@
+package abstractclassdemo;
+
+public abstract class AbstractFactory {
+
+	protected int att1;
+	protected int att2;
+	
+	public abstract void setElement();
+	public void setElement2(){
+		
+	}
+}
---
+++ abstractclassdemo/src/abstractclassdemo/ElementFactory.java
@@ -0,0 +1,11 @@
+package abstractclassdemo;
+
+public class ElementFactory extends AbstractFactory {
+
+	@Override
+	public void setElement() {
+		att1 = 3;
+
+	}
+
+}
